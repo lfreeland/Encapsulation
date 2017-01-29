19c883b10da07386172ba5eeb3105cf1cef2aaff
wesschool <lyonsw@wsesu.org>
2017-01-29 18:26:12 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -3,7 +3,7 @@
 
 #define currentYear 2017
 
-enum Grade {														//to be added to current year to find yog
+enum Grade {						//to be added to current year to find yog
 	Undefined = 0, Senior, Junior, sophomore, Freshman,
 };
 
@@ -14,20 +14,20 @@ public:
 	Student(std::string first, std::string last);
 	~Student();
 		
-	bool setEmail();												//sets email based of name
-	void setGrade(Grade g);											//sets grade
-	bool setYog();													//sets year of graduation based of year and grade
+	bool setEmail();				//sets email based of name
+	void setGrade(Grade g);				//sets grade
+	bool setYog();					//sets year of graduation based of year and grade
 	bool sendEmail(std::string text);									
 
 private:
 	std::string firstName;	
 	std::string lastName;	
 	std::string email;			
-	std::string emailEnding = "@test.com";	//school's email ending
+	std::string emailEnding = "@example.com";	//school's email ending
 	Grade grade = Undefined;
 	int yog = 0;
 
-	bool isUserValid();						//checks to make sure student is fully filled
+	bool isUserValid();				//checks to make sure student is fully filled
 
 	
 
